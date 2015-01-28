b73b3d6aa321066a1fa4b8b6c33e896627471c8b
dr.dimitru <dr.dimitru@gmail.com>
2015-01-29 01:45:42 +0300

    Add missed dep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:iron-router-helpers',
-  version: '0.3.0',
+  version: '0.3.1',
   summary: 'An iron-router helpers for quickly adding classnames to your active navigation elements.',
   git: 'https://github.com/VeliovGroup/iron-router-helpers-for-Meteor',
   documentation: 'README.md'
@@ -8,6 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
+  api.use('templating', 'client');
   api.use('iron:router@1.0.5', 'client');
   api.addFiles('ostrio:iron-router-helpers.js', 'client');
 });
