65b803d020128279fc23b3c5178b0ddc52da573c
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-05-27 02:23:20 +0300

    v0.3.9

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+Changelog
+======
+    
+ - [`v0.3.9`] Docs and deps update
+ - [`v0.3.8`] Avoid error exception if checking empty value as a route or path
+ - [`v0.3.7`] Fix bug with helpers context verify, see [#1](https://github.com/VeliovGroup/iron-router-helpers-for-Meteor/pull/1)
+ - [`v0.3.6`] Fix for server Routes
+ - [`v0.3.5`] Updated to support `iron-router` package in version `1.0.5`
+ - [`v0.3.1`] Updated to support Meteor 1.0.
--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'ostrio:iron-router-helpers',
-  version: '0.3.8',
+  version: '0.3.9',
   summary: 'An iron-router helpers for quickly adding classnames to your active navigation elements.',
   git: 'https://github.com/VeliovGroup/iron-router-helpers-for-Meteor',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+  api.versionsFrom('1.0');
   api.use('templating', 'client');
-  api.use('iron:router@1.0.5', 'client');
+  api.use('iron:router@1.0.7', 'client');
   api.addFiles('ostrio:iron-router-helpers.js', 'client');
 });
